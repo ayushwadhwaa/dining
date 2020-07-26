71d9a85f061289dcba8288636c5b892d53443059
Ayush Wadhwa <ayush_wadhwa@yahoo.com>
2020-07-27 01:02:11 +0530

    Created TabNavigator.js

---
+++ screens/TabNavigator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+const TabNavigator = props => {
+    return (
+        <Tab.Navigator>
+            
+        </Tab.Navigator>
+    );
+}
+export default TabNavigator;
