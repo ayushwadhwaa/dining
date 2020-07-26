9b374203aac99f8c75b398035e7d038c9a6d905b
Ayush Wadhwa <ayush_wadhwa@yahoo.com>
2020-07-26 21:35:38 +0530

    Created AppNavigator.js

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+const AppNavigator = props => {
+    <NavigationContainer>
+        <Stack.Navigator>
+            
+        </Stack.Navigator>
+    </NavigationContainer>
+}
+export default AppNavigator;
