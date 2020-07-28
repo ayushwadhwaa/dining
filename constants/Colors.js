4b569f2320df500e3540424ad30d607717227264
Ayush Wadhwa <ayush_wadhwa@yahoo.com>
2020-07-28 19:39:47 +0530

    Updated RestaurantDetailsScreen

--- constants/Colors.js
+++ constants/Colors.js
@@ -4,6 +4,8 @@ const Colors = {
     cultured: '#F5F5F5',
     opal: '#9AC6C5',
     greenMunsell: '#00A878',
-    richBlack : '#101119'
+    richBlack : '#101119',
+    midninghtBlue: '#140D4F',
+    grey: '#565554'
 }
 export default Colors;
--- screens/restaurant/RestaurantDetailsScreen.js
+++ screens/restaurant/RestaurantDetailsScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import {mapBoxAccessToken} from './../../config/apiKey';
 import {useSelector} from 'react-redux';
+import Colors from './../../constants/Colors';
+import { color } from 'react-native-reanimated';
 const RestaurantDetailsScreen = props => {
     const {resID} = props.route.params;
     const restaurants = useSelector(state => state.restaurant.nearbyRestaurants);
@@ -11,20 +13,64 @@ const RestaurantDetailsScreen = props => {
         }
     });
     return (
-        <View style={syles.container}>
-            <Text>{selectedRestaurant.restaurant.name}</Text>
+        <View style={styles.container}>
+            <View style={styles.warpper}>
+                <Text style={styles.resName}>{selectedRestaurant.restaurant.name}</Text>
+                <View style={styles.imageWrapper}>
+                    {props.image !="" ? <Image source={{uri: selectedRestaurant.restaurant.featured_image}} style={styles.image}/> : <View style={styles.fallBack}><Text>Image unavailable</Text></View>}
+                </View>
+                <Text style={styles.text}>Ratting: <Text style={styles.rattingText}>{selectedRestaurant.restaurant.user_rating.aggregate_rating} {selectedRestaurant.restaurant.user_rating.rating_text}</Text></Text>
+                <Text style={styles.text}>Cuisines: <Text style={styles.cuisines}>{selectedRestaurant.restaurant.cuisines}</Text></Text>
+                <Text style={styles.text}>Average Cost For Two: <Text  style={styles.cost}>{selectedRestaurant.restaurant.average_cost_for_two} {selectedRestaurant.restaurant.currency}</Text></Text>
+                <Text style={styles.text}>Address: <Text style={styles.address}>{selectedRestaurant.restaurant.location.address}</Text></Text>
+            </View>
         </View>
     );
 }
-const syles = StyleSheet.create({
+const styles = StyleSheet.create({
+    text:{
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    rattingText:{
+        color: Colors.popstar,
+        fontWeight: 'normal'
+    },
+    cuisines:{
+        color: Colors.greenMunsell,
+        fontWeight: 'normal'
+    },
+    cost:{
+        color: Colors.grey,
+        fontWeight: 'normal'
+    },
+    address:{
+        color: Colors.midninghtBlue,
+        fontWeight: 'normal'
+    },
+    imageWrapper:{
+        width: '100%',
+        height: 200
+    },  
+    warpper:{
+        backgroundColor: Colors.cultured,
+        flex: 1
+    },
     container: {
         flex:  1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        width: '100%',
+        padding: 20,
+        margin: 10,  
+    },
+    resName:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: Colors.spaceCarder,
+        marginBottom: 10
     },
     image:{
-        width:'80%',
-        height: 300
-    }
+        width: '95%',
+        height: '100%'
+    },
 });
 export default RestaurantDetailsScreen;
